eb1c0c61f26333fee429e0694c4a3dc5e1770e3d
Duncan <donken0122@gmail.com>
2025-08-19 12:58:24 +0800

    master修改11

--- src/app/test5/page.tsx
+++ src/app/test5/page.tsx
@@ -40,7 +40,7 @@ export default function Test5(){
                 <Group justify="center" gap="md">
                     <IconBrandTwitter size={32} color="#1da1f2" />
                     <Title order={1} c="blue" ta="center">
-                        Twitter 最新推文獲取18
+                        Twitter 最新推文獲取器1212
                     </Title>
                 </Group>
                 
